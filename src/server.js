ad3c45bac9c73ea29a7953efa6e99ad751c64d85
Deepred <deepred.kim@gmail.com>
2021-05-18 17:49:18 +0900

    #15.6

--- src/server.js
+++ src/server.js
@@ -4,18 +4,19 @@ const PORT = 4000;
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+  console.log(`Someone is going to: ${req.url}`);
+  next();
+}
+
 const handleHome = (req, res) => {
   return res.send("<h1>I still love you.</h1>");
 }
 
-const handleLogin = (req, res) => {
-  return res.send('Login here.');
-}
+app.get("/", gossipMiddleware, handleHome);
 
-app.get("/", handleHome);
-app.get("/login", handleLogin);
-
-const handleListening = () => console.log(`Server listening on port http:/localhost:${PORT}`);
+const handleListening = () =>
+  console.log(`Server listening on port http:/localhost:${PORT}`);
 
 app.listen(4000, handleListening);
 
