edaccc36090eaca28776318d74462a7b17dcc4fd
Deepred <deepred.kim@gmail.com>
2021-05-18 16:37:36 +0900

    #15.4

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const PORT = 4000;
 const app = express();
 
 const handleHome = (req, res) => {
-  return res.send("I still love you.");
+  return res.send("<h1>I still love you.</h1>");
 }
 
 const handleLogin = (req, res) => {
