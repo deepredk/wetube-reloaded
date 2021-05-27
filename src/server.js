3227b0194041648e578790bcc9666b99aebfd787
Deepred <deepred.kim@gmail.com>
2021-05-27 09:01:43 +0900

    과제 #3

--- src/server.js
+++ src/server.js
@@ -1,33 +1,52 @@
 import express from "express";
 
 const PORT = 4000;
-
 const app = express();
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
+const urlLogger = (req, res, next) => {
+  console.log("PATH:", req.path);
   next();
 };
 
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
+const getFormatDate = (date) => {
+  let year = date.getFullYear();
+  let month = 1 + date.getMonth();
+  month = month >= 10 ? month : "0" + month;
+
+  let day = date.getDate();
+  day = day >= 10 ? day : "0" + day;
+
+  return year + "." + month + "." + day;
+};
+
+const timeLogger = (req, res, next) => {
+  console.log("Time:", getFormatDate(new Date()));
+  next();
+};
+
+const securityLogger = (req, res, next) => {
+  if (req.protocol === "http") {
+    console.log("Insecure ❌");
   }
-  console.log("Allowed, you may continue.");
+  next();
+};
+
+const protector = (req, res, next) => {
+  if (req.url === "/protected") {
+    return res.end("<h1>Not Allowed</h1>");
+  }
   next();
 };
 
 const handleHome = (req, res) => {
-  return res.send("I love middlewares");
+  return res.send("hello");
 }
 
 const handleProtected = (req, res) => {
   return res.send("Welcome to the private lounge.");
 };
 
-app.use(logger);
-app.use(privateMiddleware);
+app.use(urlLogger, timeLogger, securityLogger, protector);
 
 app.get("/", handleHome);
 app.get("/protected", handleProtected);
@@ -36,5 +55,3 @@ const handleListening = () =>
   console.log(`Server listening on port http:/localhost:${PORT}`);
 
 app.listen(4000, handleListening);
-
- 
