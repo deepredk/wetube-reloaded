2b059c59da1bd2663bf07280f6dbb3cb9f4dc8a1
김진홍 <deepred.kim@gmail.com>
2021-06-02 12:08:48 +0900

    #6.3 Edit Video part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -25,19 +25,25 @@ const videos = [
   },
 ];
 
-export const trending = (req, res) => {  
-  return res.render("home", { pageTitle: "Home", videos, });
-}
+export const trending = (req, res) => {
+  return res.render("home", { pageTitle: "Home", videos });
+};
+
 export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video, });
-}
+  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
+};
+
 export const getEdit = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video, });
-}
-export const postEdit = (req, res) => {
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+};
 
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
 };
