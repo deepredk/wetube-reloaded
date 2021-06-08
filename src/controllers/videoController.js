42cc7e08ae9e0aeebde03f960fc53d6a560bafb8
Deepred <deepred.kim@gmail.com>
2021-06-08 14:40:12 +0900

    #6.25 Delete Video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -72,3 +72,10 @@ export const postUpload = async (req, res) => {
     });
   }
 };
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+
+  return res.redirect("/");
+};
