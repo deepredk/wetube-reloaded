11d146318c9bb5078304aae1c66f58ab39781524
Deepred <deepred.kim@gmail.com>
2021-06-08 10:27:35 +0900

    #6.19 Video Detail

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -9,8 +9,8 @@ import {
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id", watch);
-videoRouter.route("/:id/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
