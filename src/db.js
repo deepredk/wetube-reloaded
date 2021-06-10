222e8763b5c00267a0e5f2e7a1f56c01081d6415
Deepred <deepred.kim@gmail.com>
2021-06-10 10:12:24 +0900

    #7.1 Create Account part Two

--- src/db.js
+++ src/db.js
@@ -4,6 +4,8 @@ import Video from "./models/Video";
 mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
 });
 
 const db = mongoose.connection;
