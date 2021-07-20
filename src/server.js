1db6176b1c8412bc1f2afeb72aee864b1c7ea9c7
Deepred <deepred.kim@gmail.com>
2021-07-20 14:55:42 +0900

    #7.12 MongoStore

--- src/server.js
+++ src/server.js
@@ -1,24 +1,26 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
-import rootRouter from "./routers/rootRouter";
-import userRouter from "./routers/userRouter";
-import videoRouter from "./routers/videoRouter";
-import { localsMiddleware } from "./middlewares";
+import MongoStore from 'connect-mongo';
+import rootRouter from './routers/rootRouter';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
+import { localsMiddleware } from './middlewares';
 
 const app = express();
-const logger = morgan("dev");
+const logger = morgan('dev');
 
-app.set("view engine", "pug");
-app.set("views", process.cwd() + "/src/views");
+app.set('view engine', 'pug');
+app.set('views', process.cwd() + '/src/views');
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: "Hello!",
+    secret: 'Hello!',
     resave: true,
     saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: 'mongodb://127.0.0.1:27017/wetube' }),
   }),
 );
 
